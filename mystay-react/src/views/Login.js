0d92674511fc4fe8aa8c85418a30b7bdacc4d80b
b3rm3 <javibermejo8@gmail.com>
2024-04-03 19:04:30 +0200

    Header añadido

--- mystay-react/src/views/Login.js
+++ mystay-react/src/views/Login.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Header } from './Header';
 
 export const Login = () => {
     return (
-        <div>Login</div>
+        <div>
+            <Header />
+            <div>Login</div>
+        </div>
+
     )
 }
